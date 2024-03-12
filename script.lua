3dbd17710e999cd77fc3a35534b3121cfaaa9f21
o22 <65437873+OmarAbdo9440@users.noreply.github.com>
2024-03-12 22:09:26 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,7 @@
 local currentVersion = 1.00
 
 o22_ANTICHEAT_LICENSES = {
-    {1, "o22:code1", 22003, 2024, 3, 15, "o22"},
+    {1, "o22:code1", 22003, 2024, 3, 11, "o22"},
     {2, "o22:code2", 22004, 2024, 3, 16, "KariZma"},
     {3, "o22:code3", 22003, 2024, 3, 17, "NO USERNAME"},
 }

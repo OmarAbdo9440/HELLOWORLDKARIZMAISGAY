a03776e9700b460c978714bffa5c2720a63c9a80
o22 <65437873+OmarAbdo9440@users.noreply.github.com>
2024-03-28 00:14:52 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,10 +1,5 @@
-currentVersion = 1.20
-o22_ANTICHEAT_UPDATES = {
-    {"[#ffff00Fixed#FFFFFF] Anti-Fly"},
-    {"[#ffff00Fixed#FFFFFF] Anti-Spoof"},
-    {"[#00ff00Added#FFFFFF] Anti-Jetpack"},
-    {"[#00ff00Updated#FFFFFF] Anti-Executor"},
-}
+currentVersion = 1.00
+o22_ANTICHEAT_UPDATES = {}
 o22_ANTICHEAT_LICENSES = {
     {1, "o22:code1", 22003, 2024, 4, 11, "o22"},
     {2, "o22:code2", 22004, 2024, 3, 16, "KariZma"},

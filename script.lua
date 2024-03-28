f139b25f1775e2884ce6004f605c354c218120f8
o22 <65437873+OmarAbdo9440@users.noreply.github.com>
2024-03-28 04:04:26 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,7 +1,5 @@
-currentVersion = 1.20
-o22_ANTICHEAT_UPDATES = {
-    {"[#00ff00Fixed#FFFFFF] Anti-Spoof"},
-}
+currentVersion = 1.00
+o22_ANTICHEAT_UPDATES = {}
 o22_ANTICHEAT_LICENSES = {
     {3, "o22:code1", 22003, 2030, 1, 1, "o22"},
 }
